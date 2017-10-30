43a54d0cefbbe88735938a81d70e21252438b226
Noa Raz <noa@atomation.net>
2017-10-30 17:24:39 +0200

    real clone from assignment

--- sexpr-parser.scm
+++ sexpr-parser.scm
@@ -1,29 +1,5 @@
 (load "pc.scm")
 
-(define <Boolean>
-	(new (*parser #t)
-		(*parser #f)
-	(*disj 2)
-	
-	done))
-
 (define <sexpr>
-	(new (*parser <Boolean>)
-		 ;(*parser <Char>)
-		 ;(*parser <Number>)
-		 ;(*parser <String>)
-		 ;(*parser <Symbol>)
-		 ;(*parser <ProperList>)
-		 ;(*parser <ImproperList>)
-		 ;(*parser <Vector>)
-		 ;(*parser <Queted>)
-		 ;(*parser <QuasiQuoted>)
-		 ;(*parser <Unquoted>)
-		 ;(*parser <UnquoteAndSpliced>)
-		 ;(*parser <CBName>)
-		 ;(*parser <InfixExtension>)
-		(*disj 1)
-		done))
-
-
-  
+  ;; fill in the s-expression parser details here
+  )
